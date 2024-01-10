f2c73c84f439d4c5aaccc49aa31df06e55e2516f
sakai <sakai263016@gmail.com>
2024-01-10 17:21:24 +0800

    @Autowire 对象的注入

--- _03spring/src/main/java/com/sakai/spring/bean/UserService.java
+++ _03spring/src/main/java/com/sakai/spring/bean/UserService.java
@@ -4,6 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+/**
+ * Autowired注解：自动装配，注入对象和接口类型数据
+ * 1.添加到方法上
+ * 2.添加到set方法上
+ * 3.添加到构造方法上
+ */
 @Service
 public class UserService {
 
