72cd5455dd71d7d98d8b7b6792662d25618c4fb6
sakai <sakai263016@gmail.com>
2024-01-10 15:39:27 +0800

    <property> 依赖注入练习

---
+++ _03spring/src/main/java/com/sakai/spring/eg1/TestStudent.java
@@ -0,0 +1,13 @@
+package com.sakai.spring.eg1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("student.xml");
+        Student student = context.getBean(Student.class);
+        System.out.println(student);
+    }
+
+}
