f3aa3686edd11592a2c2acfa6a326a794674b58e
sakai <sakai263016@gmail.com>
2024-01-10 17:51:39 +0800

    @Autowire 多接口的注入解决方案：指定一个实现类

---
+++ _03spring/src/main/java/com/sakai/spring/auto/CacheImpl2.java
@@ -0,0 +1,7 @@
+package com.sakai.spring.auto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CacheImpl2 implements Cache {
+}
--- _03spring/src/main/java/com/sakai/spring/auto/UserCache.java
+++ _03spring/src/main/java/com/sakai/spring/auto/UserCache.java
@@ -1,10 +1,19 @@
 package com.sakai.spring.auto;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserCache {
+    /*
+    Autowired注解：只能根据类型进行装配，不能根据Bean对象的名称进行装配
+    如果体格接口有多个实现类，需要结合Qualifier注解根据Bean对象名称进行装配
+    Qualifier注解：
+        配合Autowired注解
+        value参数值得是IoC容器中Spring Bean 对象的名称
+     */
     @Autowired
+    @Qualifier(value = "cacheImpl1")
     private Cache cache;
 }
