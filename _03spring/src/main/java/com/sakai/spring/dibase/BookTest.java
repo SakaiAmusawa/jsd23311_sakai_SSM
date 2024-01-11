ad7f7ca8f03980fce18e83b197778a248fece6b6
sakai <sakai263016@gmail.com>
2024-01-11 14:02:50 +0800

    System

--- _03spring/src/main/java/com/sakai/spring/dibase/BookTest.java
+++ _03spring/src/main/java/com/sakai/spring/dibase/BookTest.java
@@ -1,6 +1,6 @@
 package com.sakai.spring.dibase;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
