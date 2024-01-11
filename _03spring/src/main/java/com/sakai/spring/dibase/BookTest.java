748118868545494f1fa9540b58f830892a41dd46
sakai <sakai263016@gmail.com>
2024-01-11 10:45:33 +0800

    部分修改

---
+++ _03spring/src/main/java/com/sakai/spring/dibase/BookTest.java
@@ -0,0 +1,14 @@
+package com.sakai.spring.dibase;
+
+import org.testng.annotations.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BookTest {
+    @Test
+    public void bookTest() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("book.xml");
+        Book book = context.getBean(Book.class);
+        System.out.println(book);
+    }
+}
