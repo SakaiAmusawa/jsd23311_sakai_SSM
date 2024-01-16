70b452739a1a824a27431ff22a57d89f3af158bd
sakai <sakai263016@gmail.com>
2024-01-16 15:55:27 +0800

    接受数据的方式小练习：参数接受和POJO类接受

--- _05mvcboot01/src/main/java/com/sakai/_05mvcboot01/controller/BMIController.java
+++ _05mvcboot01/src/main/java/com/sakai/_05mvcboot01/controller/BMIController.java
@@ -7,12 +7,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BMIController {
+
+    /**
+     * <a href="http://localhost:8080/bmi2?kg=50&high=1.75">测试链接</a>
+     */
     @RequestMapping("/bmi")
     @ResponseBody
     public String bmi(Double kg, Double high) {
         return getBMI(kg, high);
     }
 
+    /**
+     * <a href= http://localhost:8080/bmi2?kg=90&high=1.75>测试链接</a>
+     */
     @RequestMapping("/bmi2")
     @ResponseBody
     public String bmi2(BMIDTO bmidto) {
@@ -29,7 +36,7 @@ public class BMIController {
         } else if (bmi < 24) {
             result = "正常";
         } else if (bmi < 27) {
-            result = "正常";
+            result = "微胖";
         } else {
             result = "肥胖";
         }
