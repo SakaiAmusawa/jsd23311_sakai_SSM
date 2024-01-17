35ef5f102fb5ab46b6b7a1720ce2d29c1e5ba7fc
sakai <sakai263016@gmail.com>
2024-01-17 14:42:34 +0800

    @Mapper注解的优化

--- _05mvcboot02/src/main/java/com/sakai/_05mvcboot02/config/MybatisConfig.java
+++ _05mvcboot02/src/main/java/com/sakai/_05mvcboot02/config/MybatisConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration 标识此类为配置类，Spring会自动加载此类
 @MapperScan 自动扫描注解；
     1. 会自动扫描指定包路径及子孙包中的所有接口，自动为其添加@Mapper注解；
-    2. 设置
+    2. 设置自动扫描后,所有的接口上无需再添加 @Mapper 注解.
  */
 @Configuration
 @MapperScan(value = "com.sakai._05mvcboot02")
