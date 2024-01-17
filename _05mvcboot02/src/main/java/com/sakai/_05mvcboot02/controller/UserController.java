fb33bf92a31e5cc7a3b79f9e8b8fcb5f61226ff9
sakai <sakai263016@gmail.com>
2024-01-17 14:02:20 +0800

    mybatis与spring mvc的结合

--- _05mvcboot02/src/main/java/com/sakai/_05mvcboot02/controller/UserController.java
+++ _05mvcboot02/src/main/java/com/sakai/_05mvcboot02/controller/UserController.java
@@ -71,10 +71,6 @@ public class UserController {
         请求体数据：用户id、用户名username、密码password、昵称nickname、时间created
         返回响应：String "修改成功"
          */
-        Integer id = user.getId();
-        String username = user.getUsername();
-        String nickname = user.getNickname();
-        String password = user.getPassword();
         user.setCreated(new Date());
         userMapper.updateUser(user);
 
